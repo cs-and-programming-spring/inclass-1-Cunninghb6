42e3a7e78811bd8e76e2ee8d2bd3e9413a5c5634
cunninghb6 <cunninghb6@RobiLab325-07>
2019-02-05 17:36:25 -0500

    assignment 1 completed

--- Source.cpp
+++ Source.cpp
@@ -6,83 +6,88 @@
 * Email: cunninghb6.rowan.edu
 ***********************************************
 * Purpose:
-*  This program calculates area of circle
+*  Homework
 ***********************************************/
 
 #include <iostream>
 
 using namespace std; 
 
-const  float PI = 3.14159;
+
 
 int main()
 {
-
-	float miles, kilometers;
+	
+	float fahr, cel;
 	
 	//Display purpose of the program
 	cout << " ******************************************"<<endl;
-	cout << " This program converts miles to kilometers" <<endl;
+	cout << " This program converts degrees Celsius to degrees Fahrenheit" <<endl;
 	cout << " ******************************************\n\n";
 	
 	//Get user input
-	cout << "Please input miles: ";
-	cin >> miles;
+	cout << "Please input degrees Celsius: ";
+	cin >> cel;
 	
 	//Computing function
-	kilometers = miles * 1.6; 
+	fahr =  cel * 9/5 + 32 ; 
 	
-	//Display the area 
-	cout << miles <<" miles is " << kilometers << " kilometers.\n\n";
+	//Display coversion
+	cout << cel <<" degrees Celsius is " << fahr << " degrees Fahrenheit.\n\n";
 	cout << "Program has ended" << endl;
-
+	
 	//
 	//2
 	//
 
-	float radius, circumference;
+	float age, year, months, days, hours, minutes, seconds;
 
 	//Display purpose of the program
 	cout << " ******************************************" << endl;
-	cout << " This program takes in radius to compute circumference of circle" << endl;
+	cout << " This program takes your age in years and coverts it to months, days, hours, minutes and seconds." << endl;
 	cout << " ******************************************\n\n";
 
 	//Get user input
-	cout << "Please input radius: ";
-	cin >> radius;
+	cout << "Please input your age in years: ";
+	cin >> year;
 
-	//Compute area to PI*radius*radius
-	circumference = PI * radius *2;
+	//Compute conversions
+	months = year * 12;
+	days = year * 365.25;
+	hours = days *24;
+	minutes = hours * 60;
+	seconds = minutes * 60;
 
-	//Display the area 
-	cout << " The circumference is = " << circumference << "\n\n";
+	//Display the initial
+	cout << "Your age in years = " << year << "\n";
+	cout << "Your age in months = " << months << "\n";
+	cout << "Your age in days = " << days << "\n";
+	cout << "Your age in hours = " << hours << "\n";
+	cout << "Your age in minutes = " << minutes << "\n";
+	cout << "Your age in seconds = " << seconds << "\n";
+	
 	cout << "Program has ended\n" << endl;
 
 	//
 	//3
 	//
-
-	float mile, km, m, cm, mm;
+	
+	
 
 	//Display purpose of the program
 	cout << " ******************************************" << endl;
-	cout << " This program converts miles to kilometers, meters, centimeters, and milometers" << endl;
+	cout << " This program displays my first initial" << endl;
 	cout << " ******************************************\n\n";
 
-	//Get user input
-	cout << "Please input miles: ";
-	cin >> mile;
-
-	//Computing functions
-	km = mile * 1.6;
-	m = km * 1000;
-	cm = m * 100; 
-	mm = m * 1000;
+	cout << "	***   " << endl;
+	cout << "	*  *  " << endl;
+	cout << "	*** " << endl;
+	cout << "	*  *" << endl;
+	cout << "	*  *  " << endl;
+	cout << "	*** " << endl;
 
 
-	//Display the area 
-	cout << mile << " miles is " << km << " kilometers or " << m << " meters or " << cm << " centimeters or " << mm << " millimeters.\n\n";
-	cout << "Program has ended" << endl;
+	
 	
 	system("pause");
 	
